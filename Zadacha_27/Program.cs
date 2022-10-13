91e9079b9512b2fb1623072720c78cf5deef47a6
SandyAndy <ansigida77@gmail.com>
2022-10-13 21:40:54 +0300

    Задача 2

---
+++ Zadacha_27/Program.cs
@@ -0,0 +1,27 @@
+﻿int A = insertNumber("Введите число:");
+
+A = Math.Abs(A);
+
+int result = sumCifr(A);
+Console.Write("Сумма цифр числа:");
+Console.WriteLine(result);
+
+//Метод ввода данных
+int insertNumber (string message)
+{
+Console.WriteLine(message);
+int number = int.Parse(Console.ReadLine());
+return number;
+}
+
+//Метод суммы цифр числа
+int sumCifr (int N)
+{
+int sum = 0;
+while (N != 0)
+{
+    sum = sum + N%10;
+    N = N/10;
+}
+return sum;
+}
