b4a6b468d3b30f457a21b860b9a126281562ab58
SandyAndy <ansigida77@gmail.com>
2022-10-13 21:02:16 +0300

    Задача 1

---
+++ Zadacha_25/Program.cs
@@ -0,0 +1,26 @@
+﻿int A = insertNumber("Введите возводимое в степень число:");
+int B = insertNumber("Введите натуральную степень, в которую будет возводиться число:");
+
+if(B<1) Console.WriteLine("Степень должна быть натуральным числом.");
+else 
+ {
+ int stepen = funcStepen(A,B);
+Console.Write("Результат:");
+Console.WriteLine(stepen);
+}
+
+//Метод ввода данных
+int insertNumber (string message)
+{
+    Console.WriteLine(message);
+int number = int.Parse(Console.ReadLine());
+return number;
+}
+
+//Метод вычисления степени
+int funcStepen (int a, int b)
+{
+    int need = a;
+    for (int i=2; i<=b; i++) need = A*need;
+    return need;
+}
